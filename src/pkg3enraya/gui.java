df77a98529528ddf19e89473ee1f58d0235f2133
Andres Camilo Ocampo <andres_ocampo82211@elpoli.edu.co>
2023-01-05 15:01:42 -0500

    Just a bgc change

--- src/pkg3enraya/gui.java
+++ src/pkg3enraya/gui.java
@@ -54,7 +54,7 @@ public class gui extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(102, 204, 255));
+        jPanel1.setBackground(new java.awt.Color(153, 255, 255));
 
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
